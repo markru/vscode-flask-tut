59d817a21c6e6f17de584d65534ec2f9861e43e2
markru <this.markru@gmail.com>
2020-04-21 19:54:20 -0700

    refactored app for static templete

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from datetime import datetime
 import re
 
@@ -10,19 +11,7 @@ def home():
     return "Hello from Flask!!"
 
 
+@app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+def hello_there(name=None):
+    return render_template("hello_there.html", name=name, date=datetime.now())
