68e44a7630a7907d3da2093167ca6e7d538cc1e0
markru <this.markru@gmail.com>
2020-04-21 20:07:52 -0700

    added data api ... just testing how it works

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ def home():
 @app.route("/hello/<name>")
 def hello_there(name=None):
     return render_template("hello_there.html", name=name, date=datetime.now())
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
